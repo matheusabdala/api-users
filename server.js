42e32d7796b5f26ec500ab4a2ba8efb1409a7db4
Matheus Abdala <theus0702@gmail.com>
2025-02-12 14:15:08 -0400

    Adicionando CORS

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import userRoutes from './src/routes/userRoutes.js';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.use(userRoutes)
+app.use(userRoutes);
 
 app.listen(3002, '0.0.0.0', () => {
   console.log('App iniciado na porta 3002');
